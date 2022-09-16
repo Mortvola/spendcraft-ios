09d0fa6dcb4b1c5278354c89a5760ef24b312f5e
mortvola <shields12345@msn.com>
2022-09-15 19:49:50 -0700

    Initial Commit

---
+++ SpendCraft/SpendCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendCraftApp.swift
+//  SpendCraft
+//
+//  Created by Richard Shields on 9/15/22.
+//
+
+import SwiftUI
+
+@main
+struct SpendCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
